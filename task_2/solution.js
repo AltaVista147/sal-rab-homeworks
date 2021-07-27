2f2982a9440bcfafaa039f266c4f0d2afb8d9e2a
AltaVista147 <lanthir88@gmail.com>
2021-07-27 17:53:49 +0300

    task_2 done 2.0

--- task_2/solution.js
+++ task_2/solution.js
@@ -17,9 +17,7 @@ function calcShipping(sum, min, shipping) {
     // то shippingSum присвоить значение shippingPrice
 
     let shippingSum;
-    if (productsSum === 0) {
-        shippingSum = 0;
-    } else if (productsSum >= freeShippingMinSum) {
+    if (productsSum === 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
     } else if (productsSum > 0 && productsSum < freeShippingMinSum) {
         shippingSum = shippingPrice;
